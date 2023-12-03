f496dfc1059f3cbd6b97d5bfc1f0f0465cd1ebda
[zh9un] <[jyhnegis.naver.com]>
2023-12-03 19:26:59 +0900

    지현,AgreeJang내의js파일들코드수정

--- toronFront/components/AgreeJang/AgreeInput.js
+++ toronFront/components/AgreeJang/AgreeInput.js
@@ -1,22 +1,29 @@
-
 import React, { useState, useRef, useEffect } from 'react';
-import { Image, TouchableOpacity, View, Text, TextInput, ScrollView, KeyboardAvoidingView, Platform, StyleSheet, InputAccessoryView } from 'react-native';
+import { Pressable,Image, TouchableOpacity, View, Text, TextInput, ScrollView, KeyboardAvoidingView, Platform, StyleSheet, InputAccessoryView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const AgreeInput = () => {
+    useEffect(()=>{
+        Platform.OS == 'ios' ? StatusBarManager.getHeight((statusBarFrameData) => {
+            setStatusBarHeight(statusBarFrameData.height)
+          }) : null
+    }, []);
+    const [statusBarHeight, setStatusBarHeight] = useState(0);
     const initialText = '';
     const [text, setText] = useState(initialText);
     const styles = StyleSheet.create({
-        InputBox:{
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginLeft: 20,
-        },
+     
         textInput:{
             padding: 7,
             backgroundColor: 'white',
             borderRadius: 10,
-            width: 280
-        
+            width: '90%',
+            fontSize: 14,
+            paddingRight: 35,
+            height: "90%"
+        },
+        InputContainer:{
+            marginTop: "auto",
         },
         Button:{
             margin: 10
@@ -27,38 +34,35 @@ const AgreeInput = () => {
         submitText:{
             fontSize: 18,
         },
-        submitButton:{
-            width: 30,
-            height: 30,
-            marginLeft: 10
-        },
-        buttonStyle:{
-            alignItems: 'center'
+        // submitButton:{
+        //     width: 30,
+        //     height: 30,
+        //     marginLeft: 10
+        // },
+        inputStyle:{
+            flexDirection: 'row',
         }
         
     });
+    const sendMessage=()=>{
+        console.log('message: ', messageText);
+    }
    return(
-    <KeyboardAvoidingView style={styles.InputBox}>
-        <View keyboardDismissMode='interactive'>
-            <View >
+    <KeyboardAvoidingView 
+    style={styles.InputContainer}
+    behavior={'padding'}
+    keyboardVerticalOffset={statusBarHeight+50}
+    >
+        <View style={styles.inputStyle}>
              <TextInput style={styles.textInput}
              value={text}
-             placeholder={'의견을 입력해 주세요.'}
+             placeholder={'입력해 주세요.'}
               onChange={setText}
-              autoFocus={true}
-              keyboardType="numeric">
-                
+              autoCorrect={false}>
              </TextInput>
-            </View>
-            
-        </View>
-
-        <View style={styles.inputStyle}>
-            <View style={styles.right}>
-                <TouchableOpacity style={styles.buttonStyle} title="전송">
-                <Image style={styles.submitButton} source={require('../../assets/submit.png')} />
+             <TouchableOpacity title="전송">
+                <Image style={{ position: 'absolute', top: 5, left: 5, zIndex: 1, width: 30, height: 30 }} source={require('../../assets/submit.png')} />
                     </TouchableOpacity>    
-            </View>
         </View>
     </KeyboardAvoidingView>
     
--- toronFront/screens/AgreeMain.js
+++ toronFront/screens/AgreeMain.js
@@ -1,37 +1,43 @@
-
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAwareScrollView,StyleSheet, Text, View, ScrollView, Platform, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Platform, KeyboardAvoidingView, SafeAreaView } from 'react-native';
 import AgreeHeader from "../components/AgreeJang/AgreeHeader";
 import AgreeContainer from "../components/AgreeJang/AgreeContainer";
-import AgreeComment from "../components/AgreeJang/AgreeComment";
 import { LinearGradient } from 'expo-linear-gradient'
 import AgreeInput from "../components/AgreeJang/AgreeInput";
+import AgreeCommentList from "../components/AgreeJang/AgreeCommentList";
+
 // background FDC8E1: 왼쪽 끝 : 연분홍
 // CFBAFD 그 다음 : 연보라 
 // A8F1A1 그 다음 : 연두
 // C2F4FC 그 다음 : 연하늘 
-const  AgreeMain =()=> {
+
+const  AgreeMain =({navigation})=> {
+
     const styles = StyleSheet.create({
         main:{
             flex: 1,
             padding: 20,
-            
+
           },
-        agreeComment:{
-            flex: 3,
-            backgroundColor: 'pink'
-        },
         agreeContainer:{
-            flex: 2,
-            zIndex: 2
+            flex: 4,
         },
-        AgreeInputStyle:{
-            flex: 1,
-            zIndex: 1
-        }
+        agreeCommentInputContainer: {
+            flex: 4, // 댓글 목록과 인풋에 대한 비율 조정
+          },
+        
        
     })
+    // 리액트 자동 설정 상단 바 삭제 
+    React.useLayoutEffect(() => {
+        // Ensure that navigation is defined before using setOptions
+        if (navigation) {
+          navigation.setOptions({
+            headerShown: false,
+          });
+        }
+      }, [navigation]);
 
     return(
         <View style={styles.main}>
@@ -39,24 +45,25 @@ const  AgreeMain =()=> {
                 style={StyleSheet.absoluteFillObject}
                 start={{ x: 0, y: 0 }}
                 end={{ x: 1, y: 1 }}
-                ></LinearGradient>
-               <View style={styles.agreeHeader}>
-                <AgreeHeader />
-                </View>
 
-                <View style={styles.agreeContainer}><AgreeContainer /></View>
-                
-                <ScrollView 
-                 keyboardShouldPersistTaps="handled"
-                // 스크롤 바를 숨기는 속성 
-                showsVerticalScrollIndicator={false}
-                style={styles.agreeComment}>
-                    <AgreeComment />
-                    <AgreeComment />
-                </ScrollView>
-                
+            ></LinearGradient>
+
+            <View style={styles.agreeHeader}>
+                <AgreeHeader />
+            </View>
+
+            <View style={styles.agreeContainer}>
+                <AgreeContainer />
+            </View>
+
+           <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : 'height'}
+            style={styles.agreeCommentInputContainer}>
+                {/* <AgreeCommentList  style={styles.agreeCommentList}/> */}
+                <AgreeCommentList />
                 <AgreeInput style={styles.AgreeInputStyle}/>
-                
+
+            </KeyboardAvoidingView> 
+
             <StatusBar style="auto" />
 
         </View> 
