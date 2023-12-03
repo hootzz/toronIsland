fdc4d9a2a4cc8737802b41697f474b5bf3447246
cHynekim <liaco819@naver.com>
2023-12-03 21:37:36 +0900

    231203 채현 로그인 로직(userModel, passport.js, userController.js)

--- toronBack/controllers/userController.js
+++ toronBack/controllers/userController.js
@@ -1,1 +1,9 @@
 //로그인 및 인증 관련 로직
+const passport = require('passport');
+const mysql = require('../models/userModel');
+
+exports.googleLogin = passport.authenticate('google', {scope : ['profile', 'email']});
+exports.googleCallback = passport.authenticate('google', {
+    failureRedirect : '/',  //실패 시 :3000 으로, 즉 처음 로그인 화면
+    successRedirect : '/main'   //성공 시, :3000/main으로, 메인 화면으로
+});
